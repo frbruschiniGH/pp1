e3ab74a647cf9e657b701d10332af3b3413470f6
Franco B <fbruschini97@gmail.com>
2026-09-15 15:53:30 -0300

    Elimina import sin usar de Data Fixtures en Producto

--- src/Entity/Producto.php
+++ src/Entity/Producto.php
@@ -4,7 +4,7 @@ namespace App\Entity;
 
 use App\Repository\ProductoRepository;
 use Doctrine\ORM\Mapping as ORM;
-use App\DataFixtures;
+
 
 #[ORM\Entity(repositoryClass: ProductoRepository::class)]
 class Producto
